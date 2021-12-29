e64cb37477de306c6bd9e3a395517d430a904b51
Prajwol Devkota <prajwol.devkota016@gmail.com>
2021-12-29 02:56:45 +0100

    Updates

---
+++ precache-manifest.4bc844391cd92fa3e5ba23102e1dda3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63de5c6fd75f0e066e348931dbde3e5a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3e8cbc80d3b32c83c4ea",
+    "url": "/react-portfolio/static/css/main.83f8b568.chunk.css"
+  },
+  {
+    "revision": "0bbdd4898da7f8fccfc5",
+    "url": "/react-portfolio/static/js/2.8a5553c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.8a5553c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8cbc80d3b32c83c4ea",
+    "url": "/react-portfolio/static/js/main.f0aa9b8f.chunk.js"
+  },
+  {
+    "revision": "9ee8159c8fe9f4225823",
+    "url": "/react-portfolio/static/js/runtime-main.70ed37ea.js"
+  }
+]);
